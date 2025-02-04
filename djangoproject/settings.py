b127ed45b1078930dc07672242296ddf729c3ddd
punitsinha23 <punitsinha495@gmail.com>
2025-02-04 14:17:25 +0530

    added staticfiles

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -131,12 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # For your app's static files
-    BASE_DIR / 'theme/static',  # For Tailwind-generated files
-]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
