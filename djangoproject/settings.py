bc5ca1a944022724ff0b9a529bca1ff95b9889ae
punitsinha23 <punitsinha495@gmail.com>
2025-02-04 14:36:46 +0530

    added

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -131,9 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "theme/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
