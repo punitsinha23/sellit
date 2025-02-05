a1c1d0e5399d2ef1582645822f249446a33e0e89
punitsinha23 <punitsinha495@gmail.com>
2025-02-05 21:51:30 +0530

    added db

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -91,8 +91,15 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner', 
+        'PASSWORD': '1UkzdnsmKhH5',  
+        'HOST': 'ep-green-dew-a4cctqox-pooler.us-east-1.aws.neon.tech',
+        'PORT': '5432',  
+        'OPTIONS': {
+            'sslmode': 'require' 
+        },
     }
 }
 
